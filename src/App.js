0370dfe11ea26dacfb00be6c7f4361f6bca266fc
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-12-29 21:11:06 +1000

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import './App.css';
-import { useState } from 'react';
-import Square from './components/Square';
+import "./App.css";
+import { useState } from "react";
+import Square from "./components/Square";
 
 function App() {
-const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
 
+  const chooseSquare = (square) => {};
 
   return (
     <div className="App">
-      <div className='board'>
-        <div className='row'>
-          <Square val={[0]} chooseSquare={() => {alert(0)}}/>
-          <Square val={[1]} chooseSquare={() => {alert(1)}}/>
-          <Square val={[2]} chooseSquare={() => {alert(2)}}/>
+      <div className="board">
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className="row"></div>
+        <div className="row"></div>
       </div>
     </div>
   );
