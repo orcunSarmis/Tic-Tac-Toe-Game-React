225450dea43765e27b2879979740d506405c6a00
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-12-29 21:02:25 +1000

    adding square

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import Square from './components/Square';
 
 function App() {
 const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
@@ -8,7 +9,11 @@ const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   return (
     <div className="App">
       <div className='board'>
-        <div className='row'></div>
+        <div className='row'>
+          <Square val={[0]} chooseSquare={() => {alert(0)}}/>
+          <Square val={[1]} chooseSquare={() => {alert(1)}}/>
+          <Square val={[2]} chooseSquare={() => {alert(2)}}/>
+        </div>
         <div className='row'></div>
         <div className='row'></div>
       </div>
